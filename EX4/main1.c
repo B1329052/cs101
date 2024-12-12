78c0f7da89bdd6419f94a9d9daf1145b08a13caf
B1329052 <B1329052@cgu.edu.tw>
2024-12-12 14:54:14 +0800

    Create main1.c

---
+++ EX4/main1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n = 7;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (j + i > n) {
+                printf("%d ", i);
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
